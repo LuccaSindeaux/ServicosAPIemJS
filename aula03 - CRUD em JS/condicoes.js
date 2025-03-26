f3d10818d8e441936c2244fec1259f1b379851ab
Lucca Sindeaux <luccasindeaux@gmail.com>
2025-03-25 21:49:08 -0300

    aula de condicionais

---
+++ aula03 - CRUD em JS/condicoes.js
@@ -0,0 +1,49 @@
+let a,b;
+    a = 10;
+    b = "10";
+
+//  a == b --> True, pois avalia somente valores
+//  a === b --> false, pois avalia valor e TIPO de dado, no caso um inteirop não é igual a uma string
+
+// veritfica o tipo de dado da variável --> typeof(a)
+
+//Operador ternário é uma fprma simplificada de realizar um if else
+let tabordaBrother, tabordaSister;
+    tabordaBrother = "Patric";
+    tabordaSister = "Gabrieli";
+
+const tabordaVerify = (tabordaBrother === tabordaSister) 
+    ? "Os tabordas são os mesmos"  //if
+    : "Uma é a Taborda sister, o outro é o brother." //else
+
+console.log(tabordaVerify);
+
+//If e else sem ser ternário (recomendado para linhas maiores de código)
+let x = 20;
+let y = 30;
+if(x > y){
+    console;log("Condição: ", "x > y");
+} else if (x < y){
+    console.log("Condição: ", "x < y");
+} else{
+    console.log("Condição: ", "x = y");
+}
+
+//criação de laços/loops: for
+for (let i = 0; i < 5; i++){
+    console.log('i: ', i);
+}
+
+//Uso de for in --> exclusivo de arrays
+let marcasCarros = ["GM", "Fiat", "Renault"];
+for(let marca in marcasCarros){
+    console.log("índice: ", marca)
+}
+for(let carro of marcasCarros){
+    console.log("Carro: ", carro);
+}
+
+
+//Variáveis sem definição de valor
+let testeUndefined;
+console.log("Valor de testeUndefined: ", testeUndefined);
---
+++ aula03 - CRUD em JS/main.js
@@ -0,0 +1,10 @@
+//Operações básicas em JS
+let ct = 0;
+let a = ct++; //incremento no ct e então atribuição à variável a, portanto ct = 1
+console.log("valor de a: ", a)
+console.log("valor de ct: ", ct)
+
+let b = ++ct; //aqui ct já virou 1, e então ele receberá um incremete antes, é como se fosse escrito 1+ct, neste caso 2, e então o valor é atribuído à b.
+console.log("Valor de b: ", b)
+console.log("Valor de ct: ", ct)
+
