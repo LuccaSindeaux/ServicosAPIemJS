aeebe7826c7d635b49d990c85cff0daf4caa3bdb
Lucca Sindeaux <luccasindeaux@gmail.com>
2025-04-01 21:52:21 -0300

    Aula de funções e arrays

---
+++ aula04/function.js
@@ -0,0 +1,22 @@
+//Criação de funções em JS
+function somarDois(a,b){
+    return a+b;
+}
+
+console.log("O resultado da soma é:", somarDois(8,12))
+
+//O mais normal é que a função seja uma constante 
+const s = function somarDois(a,b){
+    return a+b;
+}
+
+//Forma compacta, vulgo arrow function
+const add = (a,b) => {
+    return a+b;
+}
+
+//JSON
+
+//Arrays
+
+//Métodos de Arrays
